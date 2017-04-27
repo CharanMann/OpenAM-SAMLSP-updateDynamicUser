7c4f17855fa23d5258bc988e53c3ba71c4f5cc76
CharanMann <charan.mann@forgerock.com>
2017-04-27 13:53:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# OpenAM-SAMLSP-updateDynamicUser
+
+* OpenAM SP SAML Attribute extension  <br />
+* OpenAM Dynamic user profile feature allows OpenAM (acting as SAML SP) to dynamically create users but these users attributes are not updated if they are updated on IdP side. More details in *[OPENAM-8340](https://bugster.forgerock.org/jira/browse/OPENAM-8340)* <br />
+* This extension resolves this issue. Note that a better solution may include to have something like IDM to sync attributes between IdP and SP. 
+
+Disclaimer of Liability :
+=========================
+This repository contain code created and maintained by the community of forgerock users and other open source contributors.
+In some cases the code may have been created by ForgeRock for community development subject to the license for specific
+language governing permission and limitations under the license.
+
+The code is provided on an "as is" basis, without warranty of any kind, to the fullest extent permitted by law. 
+
+ForgeRock does not warrant or guarantee the individual success developers may have in implementing the code on their 
+development platforms or in production configurations.
+
+ForgeRock does not warrant, guarantee or make any representations regarding the use, results of use, accuracy, timeliness 
+or completeness of any data or information relating to the alpha release of unsupported code. ForgeRock disclaims all 
+warranties, expressed or implied, and in particular, disclaims all warranties of merchantability, and warranties related 
+to the code, or any service or software related thereto.
+
+ForgeRock shall not be liable for any direct, indirect or consequential damages or costs of any type arising out of any 
+action taken by you or others related to the code.
+
+Pre-requisites :
+================
+* Versions used for this project: OpenAM 13.5, OpenDJ 3.5 
+1. OpenAM has been installed and configured.
+2. Maven has been installed and configured.
+
+OpenAM Configuration:
+=====================
+1. Build the custom plugin by using maven. 
+2. Navigate to Federation > Entity Providers > (SP Hosted Entity) > Assertion Processing. Specify 'org.forgerock.openam.saml2.plugins.examples.UpdateDynamicUserSPAttMapper' under Attribute Mapper. 
+3. Deploy openam-samlSP-updateDynamicUser-1.0.jar under <OpenAM-Tomcat>/webapps/openam/WEB-INF/lib
+4. Restart OpenAM
+  
+Testing:
+======== 
+* 
+
+
+
+
+* * *
+
+Copyright © 2017 ForgeRock, AS.
+
+The contents of this file are subject to the terms of the Common Development and
+Distribution License (the License). You may not use this file except in compliance with the
+License.
+
+You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+specific language governing permission and limitations under the License.
+
+When distributing Covered Software, include this CDDL Header Notice in each file and include
+the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+Header, with the fields enclosed by brackets [] replaced by your own identifying
+information: "Portions copyright [year] [name of copyright owner]".
+
+Portions Copyrighted 2017 Charan Mann
---
+++ src/main/java/org/forgerock/openam/saml2/plugins/examples/UpdateDynamicUserSPAttMapper.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright © 2017 ForgeRock, AS.
+ *
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions copyright [year] [name of copyright owner]".
+ *
+ * Portions Copyrighted 2017 Charan Mann
+ *
+ * OpenAM-SAMLSP-updateDynamicUser: Created by Charan Mann on 4/27/17 , 9:39 AM.
+ */
+
+package org.forgerock.openam.saml2.plugins.examples;
+
+import com.sun.identity.plugin.datastore.DataStoreProvider;
+import com.sun.identity.plugin.datastore.DataStoreProviderException;
+import com.sun.identity.saml2.assertion.Attribute;
+import com.sun.identity.saml2.common.SAML2Exception;
+import com.sun.identity.saml2.common.SAML2Utils;
+import com.sun.identity.saml2.plugins.DefaultSPAttributeMapper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This calls extends {@link DefaultSPAttributeMapper} to update attributes present in SAML assertion to OpenAM datastore
+ */
+public class UpdateDynamicUserSPAttMapper extends DefaultSPAttributeMapper {
+
+    private static DataStoreProvider dataStoreProvider = null;
+
+    static {
+        try {
+            dataStoreProvider = SAML2Utils.getDataStoreProvider();
+        } catch (SAML2Exception e) {
+            debug.error("UpdateDynamicUserSPAttMapper.static initialization failed " +
+                    e);
+        }
+    }
+
+    /**
+     * Constructor.
+     */
+    public UpdateDynamicUserSPAttMapper() {
+        debug.message("UpdateDynamicUserSPAttMapper.constructor");
+    }
+
+    /**
+     * Returns attribute map for the given list of <code>Attribute</code>
+     * objects. Also, updates the attributes in OpenAM database if user is already present
+     *
+     * @param attributes     list <code>Attribute</code>objects.
+     * @param userID         universal identifier or distinguished name(DN) of the user.
+     * @param hostEntityID   <code>EntityID</code> of the hosted provider.
+     * @param remoteEntityID <code>EntityID</code> of the remote provider.
+     * @param realm          realm name.
+     * @return a map of mapped attribute value pair. This map has the
+     * key as the attribute name and the value as the attribute value
+     * @throws SAML2Exception if any failure.
+     */
+    @Override
+    public Map<String, Set<String>> getAttributes(
+            List<Attribute> attributes,
+            String userID,
+            String hostEntityID,
+            String remoteEntityID,
+            String realm
+    ) throws SAML2Exception {
+        Map<String, Set<String>> samlUserAttrMap = super.getAttributes(attributes, userID, hostEntityID, remoteEntityID, realm);
+
+        try {
+            // Check if the user exists
+            if (dataStoreProvider.isUserExists(userID)) {
+                // Get user existing attributes that are present in SAML assertion
+                Map<String, Set<String>> existingUserAttrMap = dataStoreProvider.getAttributes(userID, samlUserAttrMap.keySet());
+                Map<String, Set<String>> updatedUserAttrMap = new HashMap<>();
+
+                for (Map.Entry<String, Set<String>> entry : samlUserAttrMap.entrySet()) {
+                    String attribute = entry.getKey();
+
+                    // If the attribute is not already there in User store, then add the SAML assertion values
+                    if (!existingUserAttrMap.containsKey(attribute)) {
+                        if(debug.messageEnabled()) {
+                            debug.message("UpdateDynamicUserSPAttMapper.getAttribute: New attribute in SAML assertion that will be persisted");
+                        }
+                        updatedUserAttrMap.put(attribute, samlUserAttrMap.get(attribute));
+                    }
+
+                    // If existingUserAttrMap attribute are different from samlUserAttrMap attribute values
+                    else if (!existingUserAttrMap.get(attribute).equals(samlUserAttrMap.get(attribute))) {
+                        if(debug.messageEnabled()) {
+                            debug.message("UpdateDynamicUserSPAttMapper.getAttribute: Updated attribute in SAML assertion that will be persisted");
+                        }
+                        updatedUserAttrMap.put(attribute, samlUserAttrMap.get(attribute));
+                    }
+                }
+
+                // If updatedUserAttrMap is not empty, then persist attributes
+                if (!updatedUserAttrMap.isEmpty()) {
+                    if(debug.messageEnabled()) {
+                        debug.message("UpdateDynamicUserSPAttMapper.getAttribute: Persisting user attributes");
+                    }
+                    dataStoreProvider.setAttributes(userID, updatedUserAttrMap);
+                }
+            }
+        } catch (DataStoreProviderException e) {
+            debug.error("UpdateDynamicUserSPAttMapper.getAttributes", e);
+            throw new SAML2Exception(e.getMessage());
+        }
+
+        return samlUserAttrMap;
+    }
+}
